ff7c8e494ac6e9b94966282e3cf420f0b7b3a92d
wen0523 <1545255656@qq.com>
2024-06-30 23:49:21 +0800

    6.30

--- src/ui/textarea.tsx
+++ src/ui/textarea.tsx
@@ -8,21 +8,23 @@ export default function Textarea() {
     function heightChange() {
         const theTextarea = myRef.current;
 
-        if (theTextarea.scrollHeight <= 176) {
-            theTextarea.style.height = 32 + 'px';
-            theTextarea.style.height = theTextarea.scrollHeight + 'px';
-        } else {
-            if (theTextarea.clientHeight < 176) {
-                theTextarea.style.height = 176 + 'px';
+        if (theTextarea) {
+            if (theTextarea.scrollHeight <= 176) {
+                theTextarea.style.height = 32 + 'px';
+                theTextarea.style.height = theTextarea.scrollHeight + 'px';
+            } else {
+                if (theTextarea.clientHeight < 176) {
+                    theTextarea.style.height = 176 + 'px';
+                }
+                theTextarea.scrollTop = theTextarea.scrollHeight;
             }
-            theTextarea.scrollTop = theTextarea.scrollHeight;
         }
     }
 
     return (
         <textarea
             ref={myRef}
-            style={{ resize: 'none', outline: 'none', whiteSpace: 'pre-wrap'}}
+            style={{ resize: 'none', outline: 'none', whiteSpace: 'pre-wrap' }}
             className="bg-transparent py-1 h-8 w-full my-1.5 mx-1"
             placeholder="Type your message here..."
             onChange={heightChange}
