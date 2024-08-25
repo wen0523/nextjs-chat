e1597b2407054b80ddb05e3693b33c30c175ef6b
wen0523 <1545255656@qq.com>
2024-08-25 23:02:34 +0800

    8.25

--- src/functions/handleresize.ts
+++ src/functions/handleresize.ts
@@ -1,7 +1,8 @@
 //store
+import { Dispatch } from 'redux'; // 从 redux 导入 Dispatch 类型
 import { toggleVisibility_minMenu } from '../redux-store/store'; // 导入 toggleVisibility action
 
-const handleResize = (dispatch) => {
+const handleResize = (dispatch: Dispatch) => {
     const clientWidth = document.documentElement.clientWidth || document.body.clientWidth;
     if (clientWidth > 750) {
         dispatch(toggleVisibility_minMenu());
@@ -9,7 +10,7 @@ const handleResize = (dispatch) => {
     }
 }
 
-const removeHandleResize = (dispatch) => {
+const removeHandleResize = (dispatch: Dispatch) => {
     window.removeEventListener('resize', (event) => {
         handleResize(dispatch)
     })
